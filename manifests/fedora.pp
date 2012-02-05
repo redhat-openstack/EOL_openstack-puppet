31454ac889ebf73b9d99c7ae2a66a2e57fc6f1e1
Derek Higgins <derekh@redhat.com>
2012-02-05 22:13:14 +0000

    Making manifest more generic

--- manifests/fedora.pp
+++ manifests/fedora.pp
@@ -9,14 +9,14 @@ $root_password = ''
 $rabbit_user     = 'guest'
 $rabbit_password = 'guest'
 $rabbit_vhost    = '/'
-$rabbit_host     = 'nova'
+$rabbit_host     = 'localhost'
 $rabbit_port     = '5672'
 
-$glance_api_servers = 'nova:9292'
-$glance_host        = 'nova'
+$glance_api_servers = 'localhost:9292'
+$glance_host        = 'localhost'
 $glance_port        = '9292'
 
-$api_server = 'nova'
+$api_server = 'localhost'
 
 $lock_path = '/var/lib/nova/tmp'
 
@@ -24,74 +24,68 @@ resources { 'nova_config':
   purge => true,
 }
 
-node nova {
-  class { 'mysql::server':
-    config_hash => {
-                     'bind_address' => '0.0.0.0',
-                     #'root_password' => '',
-                     #'etc_root_password' => true
-                   }
-  }
-
-  class { 'mysql::ruby': 
-    package_provider => 'yum',
-    package_name => 'ruby-mysql',
-  }
-
-
-  class { 'nova::db':
-    password      => $db_password,
-    dbname        => $db_name,
-    user          => $db_username,
-    host          => $clientcert,
-    # does glance need access?
-    allowed_hosts => ['localhost'],
-  }
-
-  class { 'nova::controller':
-    db_password => $db_password,
-    db_name => $db_name,
-    db_user => $db_username,
-    db_host => $db_host,
-
-    rabbit_password => $rabbit_password,
-    rabbit_port => $rabbit_port,
-    rabbit_userid => $rabbit_user,
-    rabbit_virtual_host => $rabbit_vhost,
-    rabbit_host => $rabbit_host,
-
-    image_service => 'nova.image.glance.GlanceImageService',
-
-    glance_api_servers => $glance_api_servers,
-    glance_host => $glance_host,
-    glance_port => $glance_port,
-
-    libvirt_type => 'qemu',
-  }
-
-  class { 'nova::compute':
-    api_server     => $api_server,
-    enabled        => true,
-   api_port       => 8773,
-   aws_address    => '169.254.169.254',
- }
-
-  # set up glance server
-  class { 'glance::api':
-    swift_store_user => 'foo_user',
-    swift_store_key => 'foo_pass',
-  }
-
-  class { 'glance::registry': }
-
-  class { 'nova::rabbitmq':
-    userid       => $rabbit_user,
-    password     => $rabbit_password,
-    port         => $rabbit_port,
-    virtual_host => $rabbit_vhost,
-  }
+class { 'mysql::server':
+  config_hash => {
+                  'bind_address' => '0.0.0.0',
+                   #'root_password' => '',
+                   #'etc_root_password' => true
+                 }
 }
 
-node default {
-  fail("could not find a matching node entry for ${clientcert}")
+class { 'mysql::ruby': 
+  package_provider => 'yum',
+  package_name => 'ruby-mysql',
 }
+
+class { 'nova::db':
+  password      => $db_password,
+  dbname        => $db_name,
+  user          => $db_username,
+  host          => $clientcert,
+  # does glance need access?
+  allowed_hosts => ['localhost'],
+}
+
+class { 'nova::controller':
+  db_password => $db_password,
+  db_name => $db_name,
+  db_user => $db_username,
+  db_host => $db_host,
+
+  rabbit_password => $rabbit_password,
+  rabbit_port => $rabbit_port,
+  rabbit_userid => $rabbit_user,
+  rabbit_virtual_host => $rabbit_vhost,
+  rabbit_host => $rabbit_host,
+
+  image_service => 'nova.image.glance.GlanceImageService',
+
+  glance_api_servers => $glance_api_servers,
+  glance_host => $glance_host,
+  glance_port => $glance_port,
+
+  libvirt_type => 'qemu',
+}
+
+class { 'nova::compute':
+  api_server     => $api_server,
+  enabled        => true,
+  api_port       => 8773,
+  aws_address    => '169.254.169.254',
+}
+
+# set up glance server
+class { 'glance::api':
+  swift_store_user => 'foo_user',
+  swift_store_key => 'foo_pass',
+}
+
+class { 'glance::registry': }
+
+class { 'nova::rabbitmq':
+  userid       => $rabbit_user,
+  password     => $rabbit_password,
+  port         => $rabbit_port,
+  virtual_host => $rabbit_vhost,
+}
+
