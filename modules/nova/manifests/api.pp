f9b5ac82864771f7e88119a5ca290e57032622c5
Dan Prince <dprince@redhat.com>
2012-02-24 21:56:26 -0500

    Update nova modules to support keystone.

--- modules/nova/manifests/api.pp
+++ modules/nova/manifests/api.pp
@@ -1,4 +1,15 @@
-class nova::api($enabled=false) {
+class nova::api(
+  $enabled=false,
+  $keystone_enabled = false,
+  $keystone_service_protocol = 'http',
+  $keystone_service_host = '127.0.0.1',
+  $keystone_service_port = '5000',
+  $keystone_auth_host = '127.0.0.1',
+  $keystone_auth_port = '35357',
+  $keystone_auth_protocol = 'http',
+  $keystone_auth_uri = 'http://127.0.0.1:5000/',
+  $keystone_admin_token = 'ADMIN'
+) {
 
   Exec['post-nova_config'] ~> Service['nova-api']
   Exec['nova-db-sync'] ~> Service['nova-api']
@@ -15,11 +26,20 @@ class nova::api($enabled=false) {
     require     => [Package["openstack-nova"], Nova_config['sql_connection'], Class['nova::db']],
   }
 
+  file { "/etc/nova/api-paste.ini":
+    ensure  => present,
+    owner   => 'nova',
+    group   => 'root',
+    mode    => 640,
+    content => template('nova/api-paste.ini.erb'),
+    require => Package["openstack-nova"]
+  }
+
   service { "nova-api":
     name => 'openstack-nova-api',
     ensure  => $service_ensure,
     enable  => $enabled,
     require => Package["openstack-nova"],
-    #subscribe => File["/etc/nova/nova.conf"]
+    subscribe => File["/etc/nova/api-paste.ini"]
   }
 }
