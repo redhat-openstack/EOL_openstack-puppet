046b7567780db68b7581eddd9e56ac93d588b26e
Dan Prince <dprince@redhat.com>
2012-04-10 13:33:23 -0400

    Add Fedora keystone w/ Qpid manifest.

---
+++ manifests/fedora_keystone_qpid.pp
@@ -0,0 +1,111 @@
+$db_host     = 'localhost'
+$db_name     = 'nova'
+$db_username = 'nova'
+$db_password = 'password'
+
+$old_root_password = ''
+$root_password = ''
+
+$glance_api_servers = 'localhost:9292'
+$glance_host        = 'localhost'
+$glance_port        = '9292'
+
+$api_server = 'localhost'
+
+$nova_admin_user = 'admin'
+$nova_project_name = 'nova'
+
+$nova_network = '192.168.0.0/24'
+$floating_network = '172.20.0.0/24'
+
+$lock_path = '/var/lib/nova/tmp'
+
+$qpid_password = 'p@ssw0rd'
+$qpid_user = 'nova_qpid'
+$qpid_realm = 'OPENSTACK'
+
+resources { 'nova_config':
+  purge => true,
+}
+
+class { 'qpid::server':
+  realm => $qpid_realm,
+}
+
+class { 'nova::qpid':
+  user => $qpid_user,
+  password => $qpid_password,
+  realm => $qpid_realm,
+}
+
+class { 'mysql::server':
+  config_hash => {
+                  'bind_address' => '0.0.0.0',
+                   #'root_password' => '',
+                   #'etc_root_password' => true
+                 }
+}
+
+class { 'mysql::ruby': 
+  package_provider => 'yum',
+  package_name => 'ruby-mysql',
+}
+
+class { 'keystone': }
+
+class { 'keystone::api': }
+
+class { 'nova::db':
+  password      => $db_password,
+  dbname        => $db_name,
+  user          => $db_username,
+  host          => $clientcert,
+  # does glance need access?
+  allowed_hosts => ['localhost'],
+}
+
+class { 'nova::controller':
+  db_password => $db_password,
+  db_name => $db_name,
+  db_user => $db_username,
+  db_host => $db_host,
+
+  image_service => 'nova.image.glance.GlanceImageService',
+
+  glance_api_servers => $glance_api_servers,
+  glance_host => $glance_host,
+  glance_port => $glance_port,
+
+  libvirt_type => 'qemu',
+
+  admin_user => $nova_admin_user,
+  project_name => $nova_project_name,
+
+  nova_network => $nova_network,
+  floating_network => $floating_network,
+  keystone_enabled => true,
+  scheduler_default_filters => 'AvailabilityZoneFilter,ComputeFilter',
+  allow_resize_to_same_host => true,
+  libvirt_wait_soft_reboot_seconds => 15,
+  require => Class["keystone"],
+  rpc_backend => 'nova.rpc.impl_qpid',
+  qpid_username => $qpid_user,
+  qpid_password => $qpid_password,
+}
+
+class { 'nova::compute':
+  api_server     => $api_server,
+  enabled        => true,
+  api_port       => 8773,
+  aws_address    => '169.254.169.254',
+}
+
+class { 'glance::api':
+  api_flavor => 'keystone+cachemanagement',
+  require => Class["keystone"]
+}
+
+class { 'glance::registry':
+  registry_flavor => 'keystone',
+  require => Class["keystone"]
+}
