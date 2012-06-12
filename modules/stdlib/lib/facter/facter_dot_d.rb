6aa96709a445b59415a83e9c4603010668d4c752
Dan Prince <dprince@redhat.com>
2012-06-12 12:10:51 -0400

    Sync with latest puppet stdlib changes.

--- modules/stdlib/lib/facter/facter_dot_d.rb
+++ modules/stdlib/lib/facter/facter_dot_d.rb
@@ -11,6 +11,7 @@
 # The cache is stored in /tmp/facts_cache.yaml as a mode
 # 600 file and will have the end result of not calling your
 # fact scripts more often than is needed
+
 class Facter::Util::DotD
     require 'yaml'
 
@@ -182,3 +183,10 @@ end
 
 Facter::Util::DotD.new("/etc/facter/facts.d").create
 Facter::Util::DotD.new("/etc/puppetlabs/facter/facts.d").create
+
+# Windows has a different configuration directory that defaults to a vendor
+# specific sub directory of the %COMMON_APPDATA% directory.
+if Dir.const_defined? 'COMMON_APPDATA' then
+  windows_facts_dot_d = File.join(Dir::COMMON_APPDATA, 'PuppetLabs', 'facter', 'facts.d')
+  Facter::Util::DotD.new(windows_facts_dot_d).create
+end
