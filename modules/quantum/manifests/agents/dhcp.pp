0740e47e274a98f8d59c0a70d98d833d16171545
Dan Prince <dprince@redhat.com>
2012-10-31 08:52:39 -0400

    More options for quantum agents.

--- modules/quantum/manifests/agents/dhcp.pp
+++ modules/quantum/manifests/agents/dhcp.pp
@@ -2,7 +2,8 @@ class quantum::agents::dhcp (
   $state_path         = "/var/lib/quantum",
   $interface_driver   = "quantum.agent.linux.interface.OVSInterfaceDriver",
   $dhcp_driver        = "quantum.agent.linux.dhcp.Dnsmasq",
-  $use_namespaces     = "False",
+  $use_namespaces     = "True",
+  $resync_interval    = "30",
   $root_helper        = "sudo /usr/bin/quantum-rootwrap /etc/quantum/rootwrap.conf"
 ) inherits quantum {
 
@@ -17,6 +18,7 @@ class quantum::agents::dhcp (
   quantum_dhcp_agent_config {
     "DEFAULT/debug":              value => $log_debug;
     "DEFAULT/state_path":         value => $state_path;
+    "DEFAULT/resync_interval":         value => $resync_interval;
     "DEFAULT/interface_driver":   value => $interface_driver;
     "DEFAULT/dhcp_driver":        value => $dhcp_driver;
     "DEFAULT/use_namespaces":     value => $use_namespaces;
--- modules/quantum/manifests/agents/l3.pp
+++ modules/quantum/manifests/agents/l3.pp
@@ -1,10 +1,11 @@
 class quantum::agents::l3 (
   $interface_driver         = "quantum.agent.linux.interface.OVSInterfaceDriver",
-  $use_namespaces           = "False",
-  $router_id                = "7e5c2aca-bbac-44dd-814d-f2ea9a4003e4",
-  $gateway_external_net_id  = "3f8699d7-f221-421a-acf5-e41e88cfd54f",
-  $metadata_ip              = "169.254.169.254",
-  $external_network_bridge  = "br-ex",
+  $use_namespaces           = "True",
+  $router_id                = "",
+  $metadata_ip              = $ipaddress,
+  $metadata_port            = "35357",
+  $external_network_bridge  = "",
+  $handle_internal_only_routers  = "True",
   $root_helper              = "sudo /usr/bin/quantum-rootwrap /etc/quantum/rootwrap.conf"
 ) inherits quantum {
   Package['quantum'] -> Quantum_l3_agent_config<||>
@@ -15,8 +16,15 @@ class quantum::agents::l3 (
     Service["quantum-server"] -> Service["quantum-l3-service"]
   }
 
+  if $gateway_external_network_id {
+    quantum_l3_agent_config {
+      "DEFAULT/gateway_external_network_id":  value => $gateway_external_network_id;
+    }
+  }
+
   quantum_l3_agent_config {
     "DEFAULT/debug":                    value => $log_debug;
+    "DEFAULT/interface_driver":         value => $interface_driver;
     "DEFAULT/auth_host":                value => $auth_host;
     "DEFAULT/auth_port":                value => $auth_port;
     "DEFAULT/auth_uri":                 value => $auth_uri;
@@ -25,9 +33,9 @@ class quantum::agents::l3 (
     "DEFAULT/admin_password":           value => $keystone_password;
     "DEFAULT/use_namespaces":           value => $use_namespaces;
     "DEFAULT/router_id":                value => $router_id;
-    "DEFAULT/gateway_external_net_id":  value => $gateway_external_net_id;
     "DEFAULT/metadata_ip":              value => $metadata_ip;
     "DEFAULT/external_network_bridge":  value => $external_network_bridge;
+    "DEFAULT/handle_internal_only_routers":  value => $handle_internal_only_routers;
     "DEFAULT/root_helper":              value => $root_helper;
   }
 
