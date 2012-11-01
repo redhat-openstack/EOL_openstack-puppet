4d54d7e7fb074216f64800151716a24899f7974c
Dan Prince <dprince@redhat.com>
2012-11-01 12:26:50 -0400

    Add support for configuring cgroup_device_acl in qemu.conf.

--- modules/nova/manifests/compute.pp
+++ modules/nova/manifests/compute.pp
@@ -2,7 +2,7 @@
 # from the virtualization implementation of the compute node
 class nova::compute(
   $enabled = true
-) {
+) inherits nova {
 
   Exec['post-nova_config'] ~> Service['nova-compute']
   Exec['nova-db-sync']  ~> Service['nova-compute']
@@ -32,6 +32,28 @@ class nova::compute(
     enable  => $enabled,
     require => [Package["openstack-nova-compute"], Service["libvirtd"]],
     before  => Exec['networking-refresh'],
+  }
+
+  if $network_api_class == 'nova.network.quantumv2.api.API' {
+    #NOTE(dprince): This can go away once we stop using type=ethernet
+    augeas { 'qemu_cgroup_device_acl':
+      context   => "/files/etc/libvirt/qemu.conf/",
+      changes => [
+        "set cgroup_device_acl/1 /dev/null",
+        "set cgroup_device_acl/2 /dev/full",
+        "set cgroup_device_acl/3 /dev/zero",
+        "set cgroup_device_acl/4 /dev/random",
+        "set cgroup_device_acl/5 /dev/urandom",
+        "set cgroup_device_acl/6 /dev/ptmx",
+        "set cgroup_device_acl/7 /dev/kvm",
+        "set cgroup_device_acl/8 /dev/kqemu",
+        "set cgroup_device_acl/9 /dev/rtc",
+        "set cgroup_device_acl/10 /dev/hpet",
+        "set cgroup_device_acl/11 /dev/net/tun",
+      ],
+      onlyif  => "get /files/etc/libvirt/qemu.conf/cgroup_device_acl/11 != '/dev/net/tun'",
+      notify  => Service['libvirtd']
+    }
   }
 
   # forward guest metadata requests to correct API server
