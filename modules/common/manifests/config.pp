ae6a3725c171b1d9f85b372524f2e86719609391
Dan Prince <dprince@redhat.com>
2013-01-17 21:47:47 -0500

    Don't upgrade to latest augeas.

--- modules/common/manifests/config.pp
+++ modules/common/manifests/config.pp
@@ -1,17 +1,17 @@
 # Class: common::config
 class common::config {
 
-  package { ['augeas', 'augeas-libs', 'ruby-augeas']:
-    ensure  => latest,
-  }
+  #package { ['augeas', 'augeas-libs', 'ruby-augeas']:
+    #ensure  => latest,
+  #}
 
   file { '/usr/share/augeas/lenses/pythonpaste.aug':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => 644,
-    source  => 'puppet:///modules/common/pythonpaste.aug',
-    require  => [Package['augeas-libs'], Package['augeas'], Package['ruby-augeas']]
+    source  => 'puppet:///modules/common/pythonpaste.aug'
+    #require  => [Package['augeas-libs'], Package['augeas'], Package['ruby-augeas']]
   }
 
 }
