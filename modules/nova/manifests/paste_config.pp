a437d591e28d775ce004a681572d36c018a956dc
Nuno Santos <nsantos@redhat.com>
2013-02-28 14:42:33 -0500

    fix syntax errors on RHEL

--- modules/nova/manifests/paste_config.pp
+++ modules/nova/manifests/paste_config.pp
@@ -2,7 +2,7 @@ define nova::paste_config(
   $context = '',
   $key = '',
   $value = '',
-  $basecontext = '/files/etc/nova/',
+  $basecontext = '/files/etc/nova/'
 ) {
 
   include 'common::config'
@@ -13,7 +13,7 @@ define nova::paste_config(
         "set $key $value",
       ],
     require => [Class["nova"], File["/usr/share/augeas/lenses/pythonpaste.aug"]],
-    onlyif  => "get $basecontext/$key != $value",
+    onlyif  => "get $basecontext/$key != $value"
   }
 
 }
