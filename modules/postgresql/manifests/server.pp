5cc1844019b937ee3ecc9c4a440a8da37003e3e4
Dan Prince <dprince@redhat.com>
2013-04-21 21:58:11 -0400

    Switch to puppetlabs postgresql.

--- modules/postgresql/manifests/server.pp
+++ modules/postgresql/manifests/server.pp
@@ -1,46 +1,70 @@
 # Class: postgresql::server
 #
-# This module manages the installation and config of the postgresql server.
-class postgresql::server(
-  $data_dir = '/var/lib/pgsql/data',
-  $package_name = 'postgresql-server',
-  $service_name = 'postgresql',
-  $hba_records = ['host    all         all         0.0.0.0/0             md5']
-) {
+# == Class: postgresql::server
+# Manages the installation of the postgresql server.  manages the package and
+# service.
+#
+# === Parameters:
+# [*package_name*] - name of package
+# [*service_name*] - name of service
+#
+# Configuration:
+#   Advanced configuration setting parameters can be placed into 'postgresql_puppet_extras.conf' (located in the same
+#   folder as 'postgresql.conf'). You can manage that file as a normal puppet file resource, or however you see fit;
+#   which gives you complete control over the settings. Any value you specify in that file will override any existing
+#   value set in the templated version.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class postgresql::server (
+  $package_name     = $postgresql::params::server_package_name,
+  $package_ensure   = 'present',
+  $service_name     = $postgresql::params::service_name,
+  $service_provider = $postgresql::params::service_provider,
+  $service_status   = $postgresql::params::service_status,
+  $config_hash      = {}
+) inherits postgresql::params {
 
-  package { $package_name:
-    ensure => present
+  package { 'postgresql-server':
+    ensure  => $package_ensure,
+    name    => $package_name,
+    tag     => 'postgresql',
   }
 
-  exec { "initdb":
-    command     => "/bin/postgresql-setup initdb",
-    creates => "${data_dir}/PG_VERSION",
-    require => Package[$package_name]
+  $config_class = {
+    'postgresql::config' => $config_hash,
   }
 
-  file { "${data_dir}/postgresql.conf":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 644,
-    content => template('postgresql/postgresql.conf.erb'),
-    subscribe => Package[$package_name],
-    require => Exec['initdb']
-  }
- 
-  file { "${data_dir}/pg_hba.conf":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 644,
-    content => template('postgresql/pg_hba.conf.erb'),
-    subscribe => Package[$package_name],
-    require => Exec['initdb']
+  create_resources( 'class', $config_class )
+
+
+  service { 'postgresqld':
+    ensure    => running,
+    name      => $service_name,
+    enable    => true,
+    require   => Package['postgresql-server'],
+    provider  => $service_provider,
+    hasstatus => true,
+    status    => $service_status,
   }
 
-  service { $service_name:
-    ensure => running,
-    subscribe => [Package[$package_name], File["${data_dir}/postgresql.conf"], File["${data_dir}/pg_hba.conf"]]
+  if ($postgresql::params::needs_initdb) {
+    include postgresql::initdb
+
+    Package['postgresql-server'] -> Class['postgresql::initdb'] -> Class['postgresql::config'] -> Service['postgresqld']
+  }
+  else  {
+    Package['postgresql-server'] -> Class['postgresql::config'] -> Service['postgresqld']
   }
 
+  exec { 'reload_postgresql':
+    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+    command     => "service ${service_name} reload",
+    onlyif      => $service_status,
+    refreshonly => true,
+  }
 }
